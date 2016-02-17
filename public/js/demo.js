953b27993797d3584d8ee50644b1b27f46224c8b
Zoie Carnegie <zoie.carnegie@gmail.com>
2016-02-17 15:09:41 -0700

    SDK Changes + Demo Changes
    - Moved config to main server file
    - Added variable passing to routes
    - removed config.js file from LR package

--- public/js/demo.js
+++ public/js/demo.js
@@ -4,8 +4,6 @@
 
 		for (var i = 0; i < navBtn.length; i++) {
 			navBtn[i].addEventListener("click", function() {
-				console.log('hello');
-				console.log(this.dataset.url);
 				window.location.href = this.dataset.url
 			});
 		};
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 /**
  * Module dependencies.
  */
+var config = {
+	apidomain: 'https://api.loginradius.com',
+	apikey: '{{ Your API KEY }}',
+	apisecret: '{{ Your API Secret }}',
+	sitename: '{{ Your Sitename }}'
+}
+
 var express = require('express')
   , routes = require('./routes/routes.js')
-  , lr = require('LoginRadius');
+  , lr = require('LoginRadius')(config);
 
 var app = module.exports = express.createServer();
 
@@ -18,16 +25,12 @@ app.configure( function() {
 	app.use( express.static( __dirname + '/public') );
 });
 
-
-
-
-
 // Routes
-app.get( '/', routes.index );
-app.post( '/callback', routes.callback );
-app.get( '/sociallogin', routes.sociallogin );
-app.get( '/userreg', routes.userreg );
-app.get( '/userreg/*', routes.userreg );
+app.get( '/', routes.index(config) );
+app.post( '/callback', routes.callback(lr) );
+app.get( '/sociallogin', routes.sociallogin(config) );
+app.get( '/userreg', routes.userreg(config) );
+app.get( '/userreg/*', routes.userreg(config) );
 
 app.listen( 3000, function() {
 	console.log( "Express server listening on port %d in %s mode", app.address().port, app.settings.env );
