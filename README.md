efb267755d8838e0caa8dab693c9c5cdf1caa959
Zoie Carnegie <zoie.carnegie@gmail.com>
2016-02-23 15:23:16 -0700

    Node SDK 3.0
    Node SDK 3.0
    -Merged from private

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# LoginRadius
-
+# LoginRadius Combined SDK for Node.js
 LoginRadius Node.js wrapper provides access to LoginRadius.
 
 LoginRadius is a unified social API platform that combines 30 major social platforms into a single simplified and maintenance-free API. With LoginRadius' API, 
@@ -8,5 +7,4 @@ websites and mobile apps can implement social login, capture user social data, e
 LoginRadius helps businesses boost user engagement on their web/mobile platform, manage online identities, utilize social media for marketing, 
 capture accurate consumer data, and get unique social insight into their customer base.
 
-
-Please visit [http://apidocs.loginradius.com/v2.0/docs/nodejs](http://apidocs.loginradius.com/v2.0/docs/nodejs) for more information.
+This repository contains a SDK and a Demo created for Node.js please see http://apidocs.loginradius.com for more details on the endpoints used.
--- demo/server.js
+++ demo/server.js
@@ -1,36 +1,37 @@
+// Config variable 
+var config = {
+	apidomain: 'https://api.loginradius.com',
+	apikey: '{{ Your API KEY }}',
+	apisecret: '{{ Your API Secret }}',
+	sitename: '{{ Your Sitename }}'
+}
 
-/**
- * Module dependencies.
- */
-
+// Module dependencies.
 var express = require('express')
-  , routes = require('./routes');
+  ,routes = require('./routes/routes.js')
+  ,lr = require('loginradius-sdk')(config);
 
 var app = module.exports = express.createServer();
 
 // Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure( function() {
+	app.set( 'views', __dirname + '/views' );
+	app.set( 'view engine', 'jade' );
+	app.use( express.bodyParser() );
+	app.use( express.methodOverride() );
+	app.use( app.router );
+	app.use( express.static( __dirname + '/public') );
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
+config.lr = lr;
 // Routes
+app.get( '/', routes.index(config) );
+app.post( '/callback', routes.callback(config) );
+app.get( '/sociallogin', routes.sociallogin(config) );
+app.get( '/userreg', routes.userreg(config) );
+app.get( '/userreg/*', routes.userreg(config) );
+app.post( '/userreg/profile', routes.userreg(config) );
 
-app.get('/', routes.index);
-app.post('/callback', routes.callback);
-
-app.listen(process.env.port || 3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen( 3000, function() {
+	console.log( "Express server listening on port %d in %s mode", app.address().port, app.settings.env );
+});
